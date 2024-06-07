551339a82e843135ac3e6aa67662ba8b41ce89b7
Siddharth S Parmar <126967589+SiddharthsParmar@users.noreply.github.com>
2024-06-07 19:08:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+// import {bulb} from './assets/assets'
+// import historyIcon from '../src/assets/assets'
+// import Sidebar from './components/Sidebar/Sidebar
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
