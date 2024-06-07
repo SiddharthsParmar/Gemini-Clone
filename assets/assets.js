1888ca1836d40c16ee05484a57425b5aa6ded3b9
Siddharth S Parmar <126967589+SiddharthsParmar@users.noreply.github.com>
2024-06-07 19:07:37 +0530

    Add files via upload

---
+++ assets/assets.js
@@ -0,0 +1,11 @@
+import bulb from './bulb.png';
+import codeIcon from './code.png'
+import compass from './compass.png'
+import history from './history.png'
+
+export const assets = {
+    bulb,
+    codeIcon,
+    compass,
+    history
+}
